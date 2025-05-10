c6b43606a8f41a9db7825c4e320f8aeb3b1ef450
giuseppecurci <giuseppecurci49@gmail.com>
2025-05-10 20:39:33 +0200

    Improved make_chart scrip

--- data/make_chart.py
+++ data/make_chart.py
@@ -8,6 +8,30 @@ def parse_args():
     parser.add_argument("--program_version", type=str, required=True, help="Path to the CSV file containing time metrics.")
     return parser.parse_args()
 
+def update_metrics(df, group, data_size, num_trees):
+    sequential_time = df.loc[
+        (df["Data Size"] == data_size) &
+        (df["Num Trees"] == num_trees) &
+        (df["Threads"] == 1),
+        "Total Time"
+    ].values[0]
+
+    for _, row in group.iterrows():
+        threads = row["Threads"]
+        total_time = row["Total Time"]
+        speedup = sequential_time / total_time
+        efficiency = speedup / threads
+
+        df.loc[
+            (df["Data Size"] == data_size) &
+            (df["Num Trees"] == num_trees) &
+            (df["Threads"] == threads),
+            ["Speedup", "Efficiency"]
+        ] = speedup, efficiency
+
+    df.to_csv(metrics_path, index=False)
+
+
 if __name__ == "__main__":
     args = parse_args()    
     metrics_path = f"../{args.program_version}/output/store_time_metrics.csv"
@@ -25,11 +49,14 @@ if __name__ == "__main__":
     # Plot Speedup
     plt.figure(figsize=(10, 6))
     for (data_size, num_trees), group in grouped:
-        data_size = round(data_size/1000000, 3)
-        plt.plot(group["Threads"], group["Speedup"], label=f"{data_size}M - {num_trees}")
+        group = group.sort_values("Threads")
+        if (group["Speedup"] == -1).any() or (group["Efficiency"] == -1).any():
+            update_metrics(df, group, data_size, num_trees)
+        data_size = round(data_size/1e6, 3)
+        plt.plot(group["Threads"], group["Speedup"], marker='o', label=f"{data_size}M - {num_trees}")
     plt.title("Speedup vs Threads")
     plt.xlim(1)
-    plt.ylim(1)
+    #plt.ylim(1)
     plt.xlabel("Threads")
     plt.ylabel("Speedup")
     plt.grid(True, linestyle="--", alpha=0.6)
@@ -40,11 +67,12 @@ if __name__ == "__main__":
     # Plot Efficiency
     plt.figure(figsize=(10, 6))
     for (data_size, num_trees), group in grouped:
+        group = group.sort_values("Threads")
         data_size = round(data_size/1000000, 3)
-        plt.plot(group["Threads"], group["Efficiency"], label=f"{data_size}M - {num_trees}")
+        plt.plot(group["Threads"], group["Efficiency"], marker='o', label=f"{data_size}M - {num_trees}")
     plt.title("Efficiency vs Threads")
     plt.xlim(1)
-    plt.ylim(0, 1)
+    #plt.ylim(0, 1.2)
     plt.xlabel("Threads")
     plt.ylabel("Efficiency")
     plt.grid(True, linestyle="--", alpha=0.6)
